4d43832d269190edae354aa7c99001b8d109508d
Kratos1700 <hunterstarsa@hotmail.com>
2020-11-08 13:11:06 +0100

    añadido codigo para mostrar registros de la bbdd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,21 @@
 import modelo.Usuarios;
 import org.hibernate.*;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         //creamos session a hibernate pasamos el config y le decimos a que clase vamos a trabajar.
-       SessionFactory miFactory =  new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Usuarios.class).buildSessionFactory();
+        SessionFactory miFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Usuarios.class).buildSessionFactory();
 
         Session miSession = miFactory.openSession();
 
         try {
             // creamos el objeto
-            Usuarios usuari = new Usuarios("Pepito", "123d","qwegddf@gmail.com");
-            Usuarios usuari2 = new Usuarios("algereu", "123erddd","qweg345ere345ddf@gmail.com");
+            Usuarios usuari = new Usuarios("Pepito", "123d", "qwegddf@gmail.com");
+            Usuarios usuari2 = new Usuarios("algereu", "123erddd", "qweg345ere345ddf@gmail.com");
             //iniciamos la transaccio
             miSession.beginTransaction();
             //guardamos el objeto a la bbdd
@@ -20,9 +23,18 @@ public class Main {
             //hacemos el commit
             miSession.getTransaction().commit();
             System.out.println("Registre guardat");
+
+
+            // codifo para mostrar los registros de la bbdd
+            Query query = miSession.createQuery("FROM Usuarios ");
+            List list = query.list();
+            for (int i=0; i<list.size();i++){
+                System.out.println(list.get(i).toString());
+            }
+
             //cerramos sesion
             miSession.close();
-        }finally {
+        } finally {
             //
             miFactory.close();
         }
